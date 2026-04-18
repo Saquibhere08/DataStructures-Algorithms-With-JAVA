a21043a075c91129264a899199710f86587d9db5
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-04-18 20:04:06 +0530

    Add files via upload

---
+++ ArrayList/Main1.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class Main1 {
+    public static void main(String[] args){
+        //Q. Store Roll number of students
+        //ArrayList
+        // Syntax:
+        //DATATYPE[] variableName=new DATATYPE[Size];
+        // eg. int [] rNo=new int[3];
+        //eg. int[] rNo={23,22,212,32,2};
+
+        int[] arr=new int[3]; //only creates the object
+        System.out.println(arr[0]); //op: 0 (as the array  is Empty [0,0,0].
+    }
+}
---
+++ ArrayList/Main2.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,63};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[6]);
+        //System.out.println(arr[7]);
+        // Provides Error as the index value starts from 0.
+        //So there is no 7th Element in the Array.
+
+    }
+}
